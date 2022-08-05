75476ed9db969562344ef07f568baba96b98edbb
Md:Soman Mia <59284020+somanmia@users.noreply.github.com>
2022-08-05 05:37:14 +0000

    Create Add Them Up.java

---
+++ TOPH ONLINE JUDGE PROBLEM SOLUTION IN JAVA/Easy Problems/Add Them Up.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main{
+  public static void  main(String[]args){
+	  Scanner input=new Scanner(System.in);
+	  int SAS,SBS,sum;
+	  SAS=input.nextInt();
+	  SBS=input.nextInt();
+	  sum=SAS+SBS;
+	  System.out.println(sum);
+		  
+  }
+	
+}
