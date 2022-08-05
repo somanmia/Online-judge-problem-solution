5d6d505f8abab1ed04ce144b8c68d61fd4713652
Md:Soman Mia <59284020+somanmia@users.noreply.github.com>
2022-08-05 05:52:49 +0000

    Update Divisors.java

--- TOPH ONLINE JUDGE PROBLEM SOLUTION IN JAVA/Easy Problems/Divisors.java
+++ TOPH ONLINE JUDGE PROBLEM SOLUTION IN JAVA/Easy Problems/Divisors.java
@@ -1,8 +1,1 @@
-import java.util.Scanner;
-public class Solution{
-	public static void main(String[]args){
-		Scanner input=new Scanner(System.in);
-		int x=input.nextInt();
-		System.out.println(x);
-	}
-}
+
