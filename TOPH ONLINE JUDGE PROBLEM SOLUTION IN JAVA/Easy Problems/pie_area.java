24db417793bb6d9129d752e10558a42e006b5bd8
Md:Soman Mia <59284020+somanmia@users.noreply.github.com>
2022-08-06 12:27:17 +0000

    Create pie_area.java

---
+++ TOPH ONLINE JUDGE PROBLEM SOLUTION IN JAVA/Easy Problems/pie_area.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Solution{
+	public static void main(String[]args){
+		Scanner input=new Scanner(System.in);
+		double r,result,  pi=3.141592653589793;
+		r=input.nextDouble();
+		result=r*r*pi;
+		System.out.println(result);
+	}
+}
