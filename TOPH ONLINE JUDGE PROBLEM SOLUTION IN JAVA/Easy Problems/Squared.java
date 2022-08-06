36c6c973a270d14b84f9a57c6f2338b73a4c676e
Md:Soman Mia <59284020+somanmia@users.noreply.github.com>
2022-08-06 12:11:24 +0000

    Create Squared.java

---
+++ TOPH ONLINE JUDGE PROBLEM SOLUTION IN JAVA/Easy Problems/Squared.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Solution{
+	public static void main(String[]args){
+		Scanner input=new Scanner(System.in);
+		int num,result;
+		num=input.nextInt();
+		result=num*num;
+		System.out.println(result);
+	}
+}
