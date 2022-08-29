55e8e498c0adc8bc3814bd34f7b8c494a4449c74
Md:Soman Mia <59284020+somanmia@users.noreply.github.com>
2022-08-29 13:33:54 +0000

    Create Mixed_fractions.c

---
+++ TOPH ONLINE JUDGE PROBLEM SOLUTION IN c/Easy Problems/Mixed_fractions.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+  int num1,num2,x,y;
+scanf("%d %d",&num1,&num2);
+          x=num1/num2;
+	      y=num1%num2;
+	      printf("%d %d %d\n",x,y,num2);
+	      
+  
+}
