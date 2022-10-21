c4c52f36f3f01067266fd943eb513c708bb3363b
Md:Soman Mia <59284020+somanmia@users.noreply.github.com>
2022-10-21 15:22:37 +0000

    Create for loop ftw.java

---
+++ TOPH ONLINE JUDGE PROBLEM SOLUTION IN JAVA/Easy Problems/for loop ftw.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Solution{
+  static void result(int n){
+      int x=n;
+       System.out.println(n+1);
+  }
+public static void main(String[]args){
+   Scanner input=new Scanner(System.in);
+   
+   result(input.nextInt());
+}
+}
